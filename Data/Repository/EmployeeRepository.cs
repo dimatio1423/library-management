96b8368be13372a982967cfed37d2507d199790d
TuNguyen123vn <nguyentu100312@gmail.com>
2023-12-03 23:33:43 +0700

    New project

---
+++ Data/Repository/EmployeeRepository.cs
@@ -0,0 +1,128 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class EmployeeRepository : RepositoryBase<Employee>
+    {
+        public EmployeeRepository() { }
+
+        public bool isUserNameExist(string username)
+        {
+            var accounts = GetAll();
+            return accounts.Any(a => a.Username == username);
+        }
+
+        public bool CheckLogin(string username, string password)
+        {
+            var accounts = GetAll();
+            foreach (var account in accounts)
+            {
+                if (account.Username == username)
+                {
+                    if (account.Password.Length != password.Length)
+                    {
+                        continue; 
+                    }
+
+                    bool passwordsMatch = true;
+                    for (int i = 0; i < password.Length; i++)
+                    {
+                        if (account.Password[i] != password[i])
+                        {
+                            passwordsMatch = false;
+                            break; 
+                        }
+                    }
+
+                    if (passwordsMatch)
+                    {
+                        return true; 
+                    }
+                }
+            }
+            return false;
+        }
+
+
+        public bool isAdmin(string username)
+        {
+            var accounts = GetAll();
+            var account = accounts.FirstOrDefault(acc => acc.Username == username);
+            if (account != null)
+            {
+                return account.EmployeeType.Equals("Admin");
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int GetRole(string username)
+        {
+            var accounts = GetAll();
+            var account = accounts.FirstOrDefault(acc => acc.Username == username);
+            if (account != null)
+            {
+                if (account.EmployeeType == "Admin") return 0;
+                else return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public string GetFullName(string username)
+        {
+            var accounts = GetAll();
+            var account = accounts.FirstOrDefault(acc => acc.Username == username);
+            if (account != null)
+            {
+                return account.FullName;
+            }
+            else
+            {
+                return "Không tìm thấy tên tài khoản";
+            }
+
+        }
+
+        public int GetEmployeeIdByName(string name)
+        {
+            var employee = GetAll().SingleOrDefault(x => x.FullName == name);
+
+            if (employee != null)
+            {
+                return employee.EmployeeId;
+            }
+            
+            return -1;
+        }
+
+        public int GetEmployeeIdByUsername(string name)
+        {
+            var employee = GetAll().FirstOrDefault(x => x.Username == name);
+
+            if (employee != null)
+            {
+                return employee.EmployeeId;
+            }
+
+            return -1;
+        }
+
+        public bool isEmpIdExisted(int employeeId)
+        {
+            var accounts = GetAll();
+            return accounts.Any(a => a.EmployeeId == employeeId);
+        }
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# library-management
+
+Dự án quản lí thư viện sử dụng C# windows form
+
+Link source: https://github.com/hoang-phuc3123/LibraryManagement
